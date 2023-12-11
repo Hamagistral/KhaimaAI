cbd9d9264899882690076aa35fb3c0aad65484ad
Hamza El Belghiti <66017329+Hamagistral@users.noreply.github.com>
2023-12-11 01:30:14 +0100

    Update 02_💬_Chatbot.py

--- streamlit/pages/02_💬_Chatbot.py
+++ streamlit/pages/02_💬_Chatbot.py
@@ -5,11 +5,10 @@ import pandas as pd
 from streamlit_chat import message
 from dotenv import load_dotenv
 
-from langchain.chat_models import ChatOpenAI
+from langchain.agents import create_csv_agent
 from langchain.chat_models import ChatOpenAI
 from langchain.agents.agent_types import AgentType
 
-from langchain_experimental.agents.agent_toolkits import create_csv_agent
 
 load_dotenv()
 
